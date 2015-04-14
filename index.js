3a399d7a61bcc5b6eac7630307ca4e3e169989ee
Steve Faulkner <southpolesteve@gmail.com>
2015-04-13 20:56:04 -0500

    Initial Commit wtih basic POC

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var app = express();
+
+app.get('/search', function (req, res) {
+  request('http://www.linkcat.info/cgi-bin/koha/opac-search.pl?idx=&limit=&q' + req.params.q, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var response = []
+      $('.searchresults')
+      res.json(response)
+    }
+  });
+});
+
+app.get('/items/:id', function(req, res){
+  request('http://www.linkcat.info/cgi-bin/koha/opac-detail.pl?biblionumber=' + req.params.id, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var response = {
+        title: $('span.displaytitle').text()
+      }
+      res.json(response)
+    }
+  });
+});
+
+app.listen(3000)
